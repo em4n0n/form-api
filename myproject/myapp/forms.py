66d808fccfac87460a53f9624c58b2224eeb359e
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-30 20:49:05 -0700

    Create an ApplicationForms class

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -4,4 +4,8 @@ class Application(forms.Form):
     name = forms.CharField(label='Name of Applicant', max_length=50)
     address = forms.CharField(label='Address', max_length=100)
     posts = (('Manager', 'Manager'), ('Cashier', 'Cashier'), ('Operator', 'Operator'))
-    field = forms.ChoiceField(choices=posts)
+    field = forms.ChoiceField(choices=posts) # ChoiceField = SELECT in SQL
+
+class ApplicationForm(forms.Form):
+    pass
+
