a7f243d9f28e0ad3671899ffd3c03833e2dfe229
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-30 20:49:42 -0700

    Rename class

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 
-class Application(forms.Form):
+class ApplicationForm(forms.Form):
     name = forms.CharField(label='Name of Applicant', max_length=50)
     address = forms.CharField(label='Address', max_length=100)
     posts = (('Manager', 'Manager'), ('Cashier', 'Cashier'), ('Operator', 'Operator'))
     field = forms.ChoiceField(choices=posts) # ChoiceField = SELECT in SQL
-
-class ApplicationForm(forms.Form):
-    pass
 
