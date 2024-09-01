0c915a06288eb19969fd7d3561393758dc964abe
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-31 20:49:59 -0700

    Create a tuple inside a tuple of shifts

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 
+SHIFTS = (
+    ("1", "Morning"),
+    ("2", "Afternoon"),
+    ("3", "Evening"),
+)
 class InputForm(forms.Form):
     first_name = forms.CharField(max_length=200)
     last_name = forms.CharField(max_length=200)
