b97153293c7255fd3fb59299479fd6f7fd5d9ac9
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-31 20:47:55 -0700

    Create a shift attribute

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -3,4 +3,5 @@ from django import forms
 class InputForm(forms.Form):
     first_name = forms.CharField(max_length=200)
     last_name = forms.CharField(max_length=200)
+    shift = forms.ChoiceField(choices = SHIFTS)
 
