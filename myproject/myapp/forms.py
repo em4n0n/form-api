462eb38ef2070f1927e649e749bd11dcff194aae
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-01 17:04:18 -0700

    Logs entry time for an employee

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -9,4 +9,5 @@ class InputForm(forms.Form):
     first_name = forms.CharField(max_length=200)
     last_name = forms.CharField(max_length=200)
     shift = forms.ChoiceField(choices = SHIFTS)
+    time_log = forms.TimeField()
 
