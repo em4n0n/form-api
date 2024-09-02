85462c46ce4eb5610536bea640d31f1a7ed5f587
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-01 21:37:01 -0700

    Enter help text for time log

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -6,8 +6,8 @@ SHIFTS = (
     ("3", "Evening"),
 )
 class InputForm(forms.Form):
-    first_name = forms.CharField(max_length=200)
+    first_name = forms.CharField(max_length=200, required=False)
     last_name = forms.CharField(max_length=200)
     shift = forms.ChoiceField(choices = SHIFTS)
-    time_log = forms.TimeField()
+    time_log = forms.TimeField(help_text="Enter the exact time")
 
